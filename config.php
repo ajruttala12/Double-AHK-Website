55d006865a6b38efa081740cc22b83df2bef1985
ajruttala12 <98562083+ajruttala12@users.noreply.github.com>
2022-05-13 17:33:41 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,5 @@
-
+<?php 
+define('DESERVER', 'localhost'); // Database Server definedef('DBUSERNAME', 'root'); // Database username define( 'DEPASSWORD", "); // Database password define('DENAME', 'demo'); // Database nane
+/* connect to MySQL database */ $db = mysqli_connect(DESERVER, DBUSERNAME, DBPASSWORD, DBNAME);
+// Check db connection if($db === false) {
+die("Error: connection error. ".mysqli_connect_error());
