4e57c3ca0d72dbeb7309f4d65d6d1d5f2550aed2
ajruttala12 <98562083+ajruttala12@users.noreply.github.com>
2022-05-16 20:18:24 -0400

    Update register.php

--- register.php
+++ register.php
@@ -1,53 +1,3 @@
-<?php
-
-require_once "config.php";
-require_once "session.php";
-
-if ($_SERVER["REQUEST_METHOOD"] == "POST" && isset($_POST['submit'])) {
-  
-  $fullname = trim($_POST['name']);
-  $email = trim($_POST['email']);  
-  $password = trim($_POST['password']);   
-  $confirm_password = trim($_POST['confirm_password']);   
-  $password_hash = password_hash($password, PASSWORD_BCRYPT);
-  
-  if($query = $db->prepare("SELECT * FROM users WHERE email = ?")) {
-    $error = '';
-    
-    $query->bind_param('s', $email);
-    $query->execute();
-    
-    $query->store_result();
-        if ($query->num_rows > 0) {
-            $error .= '<p class="error">An account with this email address already registered!</p>';
-        } else {
-          
-          if (strlen($password ) < 6 {
-            $error .= '<p class="error">Please confirm your password</p>';
-          } else {
-              if (empty($error) && ($password != $confirm_password)) {
-                $error .= '<p class "error">The Passwords do not match</p>';
-              }
-          }
-              if (empty($error) ) {
-                $insertQuery= $db->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?);");
-                $insertQuery->bind_param("sss", $fullname, $email, $password_hash);
-                $result = $insertQuery->execute();
-                if ($result) {
-                   $error .= '<p class="success">You have successfully registered for a Double AHK Account. Have fun!</p>'
-                  } else {
-                      $error .= '<p class="error">Something went wrong!</p>';
-                }
-              }
-            }
-         }
-              $query->close();
-              $insertQuery->close();
-              
-              mysqli_close($db)
-              
-              }
-            ?>
              
 
 <!DOCTYPE html>
@@ -135,6 +85,56 @@ button:hover {
 }
 </style>
 <body>
+<?php
+
+require_once "config.php";
+require_once "session.php";
+
+if ($_SERVER["REQUEST_METHOOD"] == "POST" && isset($_POST['submit'])) {
+  
+  $fullname = trim($_POST['name']);
+  $email = trim($_POST['email']);  
+  $password = trim($_POST['password']);   
+  $confirm_password = trim($_POST['confirm_password']);   
+  $password_hash = password_hash($password, PASSWORD_BCRYPT);
+  
+  if($query = $db->prepare("SELECT * FROM users WHERE email = ?")) {
+    $error = '';
+    
+    $query->bind_param('s', $email);
+    $query->execute();
+    
+    $query->store_result();
+        if ($query->num_rows > 0) {
+            $error .= '<p class="error">An account with this email address already registered!</p>';
+        } else {
+          
+          if (strlen($password ) < 6 {
+            $error .= '<p class="error">Please confirm your password</p>';
+          } else {
+              if (empty($error) && ($password != $confirm_password)) {
+                $error .= '<p class "error">The Passwords do not match</p>';
+              }
+          }
+              if (empty($error) ) {
+                $insertQuery= $db->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?);");
+                $insertQuery->bind_param("sss", $fullname, $email, $password_hash);
+                $result = $insertQuery->execute();
+                if ($result) {
+                   $error .= '<p class="success">You have successfully registered for a Double AHK Account. Have fun!</p>'
+                  } else {
+                      $error .= '<p class="error">Something went wrong!</p>';
+                }
+              }
+            }
+         }
+              $query->close();
+              $insertQuery->close();
+              
+              mysqli_close($db)
+              
+              }
+            ?>
 
 <center><form action="/action_page.php" style="border:1px solid #ccc">
   <div class="container">
